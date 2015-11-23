542d422f5ac68ef4764f9d25a424e4ae51f0a5c3
Chris Federico <cfed@cloudflare.com>
2015-11-23 11:27:05 -0800

    main: checkpoint after each download

--- main.go
+++ main.go
@@ -35,7 +35,6 @@ func main() {
 	validateFlags()
 	log.Printf("Downloading to %s", *dir)
 	downloadLogs()
-	saveCheckpoint()
 }
 
 // downloadLogs connects to log downloading service and saves
@@ -59,6 +58,8 @@ func downloadLogs() {
 			e = endT
 		}
 		saveLogs(s, e)
+		// saves the checkpoint file after the file is successfully downloaded
+		saveCheckpoint(e)
 		s = s.Add(fileInterval)
 	}
 }
@@ -99,7 +100,7 @@ func saveLogs(s, e time.Time) {
 
 // saveCheckpoint saves the last downloaded state in a file
 // to resume download from.
-func saveCheckpoint() {
+func saveCheckpoint(t time.Time) {
 	fp := filepath.Join(*dir, checkpointFile)
 	os.Remove(fp)
 	f, err := os.Create(fp)
@@ -107,7 +108,7 @@ func saveCheckpoint() {
 		log.Fatalf("Failed to create checkpoint file (%s): %v", fp, err)
 	}
 	defer f.Close()
-	if _, err := f.WriteString(strconv.FormatInt(*end, 10)); err != nil {
+	if _, err := f.WriteString(strconv.FormatInt(t.Unix(), 10)); err != nil {
 		log.Fatalf("Failed to write to checkpoint file (%s): %v", fp, err)
 	}
 }
